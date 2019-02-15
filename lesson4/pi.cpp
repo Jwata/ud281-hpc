0719cc9fb24a7eaec645867a0789818651d157c3
Jwata <watanabe0621@gmail.com>
2019-02-16 01:33:41 +0900

    pi calculation

---
+++ lesson4/pi.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <omp.h>
+
+float random_number() {
+  float v = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+  int s = (rand() % 2 == 0) ? 1 : -1;
+  return s * v;
+}
+
+int main() {
+  int N = 10000000;
+  int cnt = 0;
+  double r = 1.0;
+  double x, y;
+  srand(std::time(NULL));
+#pragma omp parallel for private(x, y) reduction(+:cnt)
+  for (int i = 0; i < N; ++i) {
+    x = random_number();
+    y = random_number();
+    cnt += int(x * x + y * y < r * r);
+  }
+
+  printf("circle points = %d\n", cnt);
+  double pi = 4.0 * cnt / N;
+  printf("pi = %f\n", pi);
+}
